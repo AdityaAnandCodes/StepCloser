16c56a5b88e6b1fd180eab10a392cd6c308ad4c2
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-12-29 20:39:56 +0530

    removed Status Code

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,6 @@ const authenticate = (req, res, next) => {
 
 app.get("/",(req,res)=>{
   res.send("Hello");
-  res.sendStatus(200);
 })
 // Route to get goals (public and user-specific goals)
 app.get("/goals", async (req, res) => {  // Remove authenticate middleware
